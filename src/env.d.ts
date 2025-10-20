144dd0521b6296852bcc9aefd68684a396568516
UnrealMan187 <p.ohrner89@gmail.com>
2025-10-20 20:33:20 +0200

    chore(vscode): enable Volar take-over, align tsconfig; fix editor diagnostics

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,6 @@
 /// <reference types="vite/client" />
-
-declare module '*.vue' {
-    import type { DefineComponent } from 'vue';
-    const component: DefineComponent<{}, {}, any>;
-    export default component;
-  }
-  
+declare module "*.vue" {
+  import type { DefineComponent } from "vue-router";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,15 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
+
 
 const routes: RouteRecordRaw[] = [
-  { path: '/', name: 'dashboard', component: () => import('../views/DashboardView.vue') },
-  { path: '/login', name: 'login', component: () => import('../views/LoginView.vue') },
-];
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
-
-export default router;
+    { path: '/', name: 'dashboard', component: () => import('../views/DashboardView.vue') },
+    { path: '/login', name: 'login', component: () => import('../views/LoginView.vue') },
+  ];
+  
+  const router = createRouter({
+    history: createWebHistory(),
+    routes,
+  });
+  
+  export default router;
+  
